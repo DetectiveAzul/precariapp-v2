a5d2f51a7412e0c8f6f47cbe4b60cdb8a508aaf2
Jaime Lopez <detec.azul@gmail.com>
2018-12-17 10:44:35 +0000

    Adding reducers to swap visibility filters and re-ordering tickets

--- src/redux/actions/tickets_actions.js
+++ src/redux/actions/tickets_actions.js
@@ -1,6 +1,17 @@
 //CONSTANTS
 export const ADD_TICKET = 'ADD_TICKET';
 export const LOAD_TICKETS = 'LOAD_TICKETS';
+export const SWAP_FILTER = 'SWAP_FILTER';
+export const VISIBILITY_FILTERS = {
+	ALL: 'ALL',
+	WAITING: 'WAITING',
+	FINISHED: 'FINISHED',
+	OPEN: 'OPEN',
+	PENDING: 'PENDING',
+	SOLVED: 'SOLVED',
+	CLOSED: 'CLOSED',
+}
+export const REVERSE_ORDER = 'REVERSE_ORDER'
 
 //ACTIONS
 
@@ -10,4 +21,12 @@ export function addTicket(ticket) {
 
 export function loadTickets(tickets) {
 	return { type: LOAD_TICKETS, tickets: tickets };
+}
+
+export function swapFilter(filter) {
+	return { type: SWAP_FILTER, filter: filter};
+}
+
+export function reverseOrder() {
+	return { type: REVERSE_ORDER }
 }
--- src/redux/reducers/tickets_reducer.js
+++ src/redux/reducers/tickets_reducer.js
@@ -1,6 +1,7 @@
 import {
 	ADD_TICKET,
 	LOAD_TICKETS,
+	REVERSE_ORDER,
 } from '../actions/tickets_actions.js';
 
 function tickets(state = [], action) {
@@ -9,6 +10,9 @@ function tickets(state = [], action) {
 		return [...state, action.ticket];
 	case LOAD_TICKETS:
 		return [...action.tickets];
+	case REVERSE_ORDER:
+		const newState = Object.assign({}, state);
+		return [...newState.reverse];
 	default:
 		return state;
 	}
